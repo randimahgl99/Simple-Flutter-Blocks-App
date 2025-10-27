a7a09fc8d6bdc9bd9f0d5702c60a3fd624d7aadc
Randima_HGL <randimahgl99@gmail.com>
2025-10-27 12:53:58 +0530

    add amall container

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project1/widgets/small_container.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,70 +19,46 @@ class HomePage extends StatelessWidget {
             fontWeight: FontWeight.w600,
           ),),
         ),
-        body: Column(
-          children: [
-            Text('User interface with flutter',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-              fontWeight: FontWeight.w600
-            ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.green.shade600
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(30),
-                child: const Text('A Flutter Container is a widget used to hold, style, and position other widgets. It can have padding, margin, color, borders, size, and alignment. Think of it as a box that you can decorate and place anywhere in your layout.',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500
-                ),),
-              ),
-            ),
-            Row(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 12),
+            child: Column(
               children: [
+                Text('User interface with flutter',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w600
+                ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
                 Container(
-                  width: 180,
-                  height: 170,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
-                    color: Colors.purpleAccent,
+                    color: Colors.green.shade600
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Open -source',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600
-                        ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text('A Flutter Container is a widget used to hold, style, and position other widgets.',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600
-                        ),
-                        ),
-                      ],
-                    ),
+                    padding: const EdgeInsets.all(30),
+                    child: const Text('A Flutter Container is a widget used to hold, style, and position other widgets. It can have padding, margin, color, borders, size, and alignment. Think of it as a box that you can decorate and place anywhere in your layout.',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500
+                    ),),
                   ),
                 ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  children: [
+                    SmallContainer()
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       );
   }
